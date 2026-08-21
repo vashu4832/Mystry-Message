5c49f0975c0c6042166cbef99f69409ac23ff9d6
ashu <ashutoshvishwakarma222@gmail.com>
2026-08-21 09:14:29 +0530

    updated dbConnection code

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,28 +1,70 @@
+// import "dotenv/config";
+// import dns from "dns";
+
+// dns.setServers(["1.1.1.1", "8.8.4.4"]);
+// import mongoose from "mongoose";
+// type ConnectionObject = {
+//     isConnected?: number
+// }
+
+// const connection: ConnectionObject = {}
+
+// async function dbConnect(): Promise<void>{
+//     if(connection.isConnected) {
+//         console.log("Already connected to DB")
+//         return
+//     }
+
+//     try {
+//         const db = await mongoose.connect(process.env.MONGODB_URI || '', {})
+//         connection.isConnected = db.connection.readyState
+
+//         console.log("DB connected")
+
+//     } catch (error) {
+//         console.log("DB connection failed",error);
+//         process.exit(1);
+//     }
+// }
+
+// export default dbConnect;
+
+// import dns from "dns";
+// dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 import mongoose from "mongoose";
 
-type ConnectionObject = {
-    isConnected?: number
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongooseConn: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  } | undefined;
 }
 
-const connection: ConnectionObject = {}
+const cached = global._mongooseConn ?? { conn: null, promise: null };
+global._mongooseConn = cached;
 
-async function dbConnect(): Promise<void>{
-    if(connection.isConnected) {
-        console.log("Already connected to DB")
-        return
-    }
+async function dbConnect(): Promise<typeof mongoose> {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-    try {
-        const db = await mongoose.connect(process.env.MONGO_URI || '', {})
-        
-        connection.isConnected = db.connection.readyState
+  if (!cached.promise) {
+    const uri = process.env.MONGODB_URI;
+    if (!uri) throw new Error("MONGODB_URI is not defined");
 
-        console.log("DB connected")
+    cached.promise = mongoose.connect(uri, {}).then((m) => m);
+  }
 
-    } catch (error) {
-        console.log("DB connection failed",error);
-        process.exit(1);
-    }
+  try {
+    cached.conn = await cached.promise;
+  } catch (err) {
+    cached.promise = null; // reset so next call can retry
+    throw err;
+  }
+
+  return cached.conn;
 }
 
 export default dbConnect;
