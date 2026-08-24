7e12d9e5f14b6c72eb9cf53d552d7fe3a8e26156
ashu <ashutoshvishwakarma222@gmail.com>
2026-08-24 20:06:17 +0530

    updated sign-up debounce

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,84 +1,90 @@
-'use client'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Controller, useForm } from "react-hook-form"
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
 
-import * as z from "zod"
-import Link from "next/link"
-import { useEffect, useState } from "react"
-import { useDebounceValue } from 'usehooks-ts'
-import { toast } from "@/components/ui/toast"
-import { useRouter } from "next/navigation"
-import { signUpSchema } from "@/schemas/signUpSchema"
-import axios, { AxiosError} from 'axios'
-import { ApiResponse } from "@/types/ApiResponse"
+import * as z from "zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useDebounceValue, useDebounceCallback } from "usehooks-ts";
+import { toast } from "@/components/ui/toast";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
 
-import { Loader2 } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import { CardContent } from "@/components/ui/card"
-import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+import { Loader2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { CardContent } from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
 
 function Page() {
-
-  const [username, setUsername] = useState('');
-  const [usernameMessage, setUsernameMessage] = useState('');
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
   const [isCheckingUsername, setIsCheckingUsername] = useState(false);
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
-  const [debouncedUsername] = useDebounceValue(username, 300)
+  const debounced = useDebounceCallback(setUsername, 400);
 
   // zod implementation
   const form = useForm({
     resolver: zodResolver(signUpSchema),
     defaultValues: {
-      username: '',
-      email: '',
-      password: ''
-    }
-  })
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
   useEffect(() => {
     const checkUsernameUnique = async () => {
-      if (debouncedUsername) {
-        setIsCheckingUsername(true)
-        setUsernameMessage('')
+      if (username) {
+        setIsCheckingUsername(true);
+        setUsernameMessage("");
         try {
-          const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`)
-          setUsernameMessage(response.data.message)
+          const response = await axios.get(
+            `/api/check-username-unique?username=${username}`,
+          );
+          setUsernameMessage(response.data.message);
         } catch (error) {
-          const axiosError = error as AxiosError<ApiResponse>
+          const axiosError = error as AxiosError<ApiResponse>;
           setUsernameMessage(
-            axiosError.response?.data.message ?? "Error checking username"
-          )
+            axiosError.response?.data.message ?? "Error checking username",
+          );
         } finally {
-          setIsCheckingUsername(false)
+          setIsCheckingUsername(false);
         }
       }
-    }
-    checkUsernameUnique()
-  }, [debouncedUsername])
+    };
+    checkUsernameUnique();
+  }, [username]);
 
   const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
-    setIsSubmitting(true)
+    setIsSubmitting(true);
     try {
-      const response = await axios.post<ApiResponse>('/api/sign-up', data)
+      const response = await axios.post<ApiResponse>("/api/sign-up", data);
       toast.add({
-        title: 'Success',
-        description: response.data.message
-      })
-      router.replace(`/verify/${username}`)
-      setIsSubmitting(false)
+        title: "Success",
+        description: response.data.message,
+      });
+      router.replace(`/verify/${username}`);
+      setIsSubmitting(false);
     } catch (error) {
       console.error("Error in signup of user", error);
       const axiosError = error as AxiosError<ApiResponse>;
-      let errorMessage = axiosError.response?.data.message
+      let errorMessage = axiosError.response?.data.message;
       toast.add({
         title: "Signup failled",
         description: errorMessage,
-      })
-      setIsSubmitting(false)
+      });
+      setIsSubmitting(false);
     }
-  }
+  };
 
   return (
     <>
@@ -91,113 +97,117 @@ function Page() {
             <p className="mb-4">Signup to start your anonymous adventure</p>
           </div>
           <CardContent>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6" >
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
               <FieldGroup>
                 {/* Username */}
-                <Controller 
+                <Controller
                   name="username"
                   control={form.control}
-                  render={({ field, fieldState}) => {
+                  render={({ field, fieldState }) => {
                     return (
                       <Field data-invalid={fieldState.invalid}>
-                          <FieldLabel htmlFor="username">
-                            Username
-                          </FieldLabel>
-                          <Input
-                            {...field}
-                            aria-invalid={fieldState.invalid}
-                            placeholder="username"
-                            id="username"
-                            autoComplete="off"
-                            onChange={(e) => {
-                              field.onChange(e)
-                              setUsername(e.target.value)
-                            }}
-                          />
-                          {fieldState.invalid && (
-                            <FieldError errors={[fieldState.error]} />
-                          )}
+                        <FieldLabel htmlFor="username">Username</FieldLabel>
+                        <Input
+                          {...field}
+                          aria-invalid={fieldState.invalid}
+                          placeholder="username"
+                          id="username"
+                          autoComplete="off"
+                          onChange={(e) => {
+                            field.onChange(e);
+                            debounced(e.target.value);
+                          }}
+                        />
+                        {isCheckingUsername && (
+                          <Loader2 className="animate-spin" />
+                        )}
+                        <p
+                          className={`text-sm ${usernameMessage === "Username is unique" ? 'text-green-500' : 'text-red-500'}`}
+                        >
+                          test {usernameMessage}
+                        </p>
+                        {fieldState.invalid && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
                       </Field>
-                    )
+                    );
                   }}
-                /> 
-
+                />
 
                 {/* Email */}
-                <Controller 
+                <Controller
                   name="email"
                   control={form.control}
-                  render={({ field, fieldState}) => {
+                  render={({ field, fieldState }) => {
                     return (
                       <Field data-invalid={fieldState.invalid}>
-                          <FieldLabel htmlFor="email">
-                            Email
-                          </FieldLabel>
-                          <Input
-                            {...field}
-                            aria-invalid={fieldState.invalid}
-                            placeholder="email"
-                            autoComplete="off"
-                            id="email"
-                          />
-                          {fieldState.invalid && (
-                            <FieldError errors={[fieldState.error]} />
-                          )}
+                        <FieldLabel htmlFor="email">Email</FieldLabel>
+                        <Input
+                          {...field}
+                          aria-invalid={fieldState.invalid}
+                          placeholder="email"
+                          autoComplete="off"
+                          id="email"
+                        />
+                        {fieldState.invalid && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
                       </Field>
-                    )
+                    );
                   }}
-                /> 
-
+                />
 
                 {/* Password */}
-                <Controller 
+                <Controller
                   name="password"
                   control={form.control}
-                  render={({ field, fieldState}) => {
+                  render={({ field, fieldState }) => {
                     return (
                       <Field data-invalid={fieldState.invalid}>
-                          <FieldLabel htmlFor="password">
-                            Password
-                          </FieldLabel>
-                          <Input
-                            {...field}
-                            aria-invalid={fieldState.invalid}
-                            placeholder="password"
-                            autoComplete="off"
-                            type="password"
-                            id="password"
-                          />
-                          {fieldState.invalid && (
-                            <FieldError errors={[fieldState.error]} />
-                          )}
+                        <FieldLabel htmlFor="password">Password</FieldLabel>
+                        <Input
+                          {...field}
+                          aria-invalid={fieldState.invalid}
+                          placeholder="password"
+                          autoComplete="off"
+                          type="password"
+                          id="password"
+                        />
+                        {fieldState.invalid && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
                       </Field>
-                    )
+                    );
                   }}
-                /> 
+                />
               </FieldGroup>
               <Button type="submit" disabled={isSubmitting}>
-                {
-                  isSubmitting ? (
-                    <>
-                      <Loader2 className="mr-2 h-4 w-4 animate-spin"/> Please wait
-                    </>
-                  ) : ('Signup')
-                }
+                {isSubmitting ? (
+                  <>
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please
+                    wait
+                  </>
+                ) : (
+                  "Signup"
+                )}
               </Button>
             </form>
           </CardContent>
           <div className="text-center mt-4">
-              <p>
-                Already a member?{' '}
-                <Link href="/sign-in" className="text-blue-600 hover:text-blue-900">
-                  Sign in
-                </Link>
-              </p>
+            <p>
+              Already a member?{" "}
+              <Link
+                href="/sign-in"
+                className="text-blue-600 hover:text-blue-900"
+              >
+                Sign in
+              </Link>
+            </p>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Page
+export default Page;
