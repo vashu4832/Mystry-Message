08c0cd3384744fbc0522b0b45a4a367092e71cbf
ashu <ashutoshvishwakarma222@gmail.com>
2026-08-24 11:00:39 +0530

    added signup in right file

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,203 @@
+'use client'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
+
+import * as z from "zod"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { useDebounceValue } from 'usehooks-ts'
+import { toast } from "@/components/ui/toast"
+import { useRouter } from "next/navigation"
+import { signUpSchema } from "@/schemas/signUpSchema"
+import axios, { AxiosError} from 'axios'
+import { ApiResponse } from "@/types/ApiResponse"
+
+import { Loader2 } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { CardContent } from "@/components/ui/card"
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+function Page() {
+
+  const [username, setUsername] = useState('');
+  const [usernameMessage, setUsernameMessage] = useState('');
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const router = useRouter();
+  const [debouncedUsername] = useDebounceValue(username, 300)
+
+  // zod implementation
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: ''
+    }
+  })
+
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true)
+        setUsernameMessage('')
+        try {
+          const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`)
+          setUsernameMessage(response.data.message)
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>
+          setUsernameMessage(
+            axiosError.response?.data.message ?? "Error checking username"
+          )
+        } finally {
+          setIsCheckingUsername(false)
+        }
+      }
+    }
+    checkUsernameUnique()
+  }, [debouncedUsername])
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true)
+    try {
+      const response = await axios.post<ApiResponse>('/api/sign-up', data)
+      toast.add({
+        title: 'Success',
+        description: response.data.message
+      })
+      router.replace(`/verify/${username}`)
+      setIsSubmitting(false)
+    } catch (error) {
+      console.error("Error in signup of user", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = axiosError.response?.data.message
+      toast.add({
+        title: "Signup failled",
+        description: errorMessage,
+      })
+      setIsSubmitting(false)
+    }
+  }
+
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+          <div className="text-center">
+            <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+              Join Mystry Message
+            </h1>
+            <p className="mb-4">Signup to start your anonymous adventure</p>
+          </div>
+          <CardContent>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6" >
+              <FieldGroup>
+                {/* Username */}
+                <Controller 
+                  name="username"
+                  control={form.control}
+                  render={({ field, fieldState}) => {
+                    return (
+                      <Field data-invalid={fieldState.invalid}>
+                          <FieldLabel htmlFor="username">
+                            Username
+                          </FieldLabel>
+                          <Input
+                            {...field}
+                            aria-invalid={fieldState.invalid}
+                            placeholder="username"
+                            id="username"
+                            autoComplete="off"
+                            onChange={(e) => {
+                              field.onChange(e)
+                              setUsername(e.target.value)
+                            }}
+                          />
+                          {fieldState.invalid && (
+                            <FieldError errors={[fieldState.error]} />
+                          )}
+                      </Field>
+                    )
+                  }}
+                /> 
+
+
+                {/* Email */}
+                <Controller 
+                  name="email"
+                  control={form.control}
+                  render={({ field, fieldState}) => {
+                    return (
+                      <Field data-invalid={fieldState.invalid}>
+                          <FieldLabel htmlFor="email">
+                            Email
+                          </FieldLabel>
+                          <Input
+                            {...field}
+                            aria-invalid={fieldState.invalid}
+                            placeholder="email"
+                            autoComplete="off"
+                            id="email"
+                          />
+                          {fieldState.invalid && (
+                            <FieldError errors={[fieldState.error]} />
+                          )}
+                      </Field>
+                    )
+                  }}
+                /> 
+
+
+                {/* Password */}
+                <Controller 
+                  name="password"
+                  control={form.control}
+                  render={({ field, fieldState}) => {
+                    return (
+                      <Field data-invalid={fieldState.invalid}>
+                          <FieldLabel htmlFor="password">
+                            Password
+                          </FieldLabel>
+                          <Input
+                            {...field}
+                            aria-invalid={fieldState.invalid}
+                            placeholder="password"
+                            autoComplete="off"
+                            type="password"
+                            id="password"
+                          />
+                          {fieldState.invalid && (
+                            <FieldError errors={[fieldState.error]} />
+                          )}
+                      </Field>
+                    )
+                  }}
+                /> 
+              </FieldGroup>
+              <Button type="submit" disabled={isSubmitting}>
+                {
+                  isSubmitting ? (
+                    <>
+                      <Loader2 className="mr-2 h-4 w-4 animate-spin"/> Please wait
+                    </>
+                  ) : ('Signup')
+                }
+              </Button>
+            </form>
+          </CardContent>
+          <div className="text-center mt-4">
+              <p>
+                Already a member?{' '}
+                <Link href="/sign-in" className="text-blue-600 hover:text-blue-900">
+                  Sign in
+                </Link>
+              </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Page
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toast"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     > 
       <AuthProvider>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Toaster />
+      </body>
       </AuthProvider>
     </html>
   );
