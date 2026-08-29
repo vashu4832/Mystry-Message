83736421868067baa53ae0a98d572bbe26cf7346
ashu <ashutoshvishwakarma222@gmail.com>
2026-08-29 15:14:35 +0530

    updated

--- app/api/delete-message/[messageid]/route.ts
+++ app/api/delete-message/[messageid]/route.ts
@@ -4,7 +4,7 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import { User } from "next-auth";
 
-export async function DELETE(request: Request, {params}: {params: {messageid:string}}) {
+export async function DELETE(request: Request, {params}: { params: Promise<{ messageid: string }> }) {
     const { messageid } = await params
     await dbConnect();
 
